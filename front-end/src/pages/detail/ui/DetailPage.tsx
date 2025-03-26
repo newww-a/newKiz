d358cf147a90d40d31329fb6b6b92e38da19333b
newww-a <tls001217@naver.com>
2025-03-27 00:49:55 +0900

    style: 디테일 페이지 ui 디자인 구현

--- front-end/src/pages/detail/ui/DetailPage.tsx
+++ front-end/src/pages/detail/ui/DetailPage.tsx
@@ -62,29 +62,53 @@ export default function DetailPage() {
 
         {/* content */}
         <div className='mt-5'>
-
-          {/* title */}
           <div className='font-extrabold text-xl sm:text-3xl'> {title} </div>
           <div className='mt-2 text-[#757575]'> {updateDay} | {reporterName} </div>
-          <div className='flex items-center'>
-            <img src="https://newkiz.s3.ap-northeast-2.amazonaws.com/dinos/nico.png" alt="character_nico" className='w-20 m-3'/>
-            <div>
+          <div className="flex items-center mx-4 mt-4">
+            <img src="https://newkiz.s3.ap-northeast-2.amazonaws.com/dinos/nico.png" alt="character_nico" className='w-20 m-1 '/>
+            <div className="relative w-full">
               <button
                 onClick={() => setIsAccordionOpen(!isAccordionOpen)}
-                className="border-2 border-[#D9D9D9] p-2 rounded-xl bg-[#F4F4F8] text-[#757575] w-full flex flex-col items-start"
+                className="border-2 border-[#D9D9D9] p-2 rounded-ms bg-[#F4F4F8] text-[#757575] w-full flex flex-col items-start"
               >
+                {/* 아코디언 버튼 */}
                 <div className="flex justify-between w-full text-xl p-2">
                   <span>기사를 보기 전에 보면 좋을 거 같아요!</span>
                   <span>{isAccordionOpen ? <LuChevronUp size={25}/> : <LuChevronDown size={25}/>}</span>
                 </div>
+                
+                {/* 기사 보기 전 내용 */}
                 {isAccordionOpen && (
-                  <div className="bg-white text-[#202020] mt-2 p-3 w-full text-left rounded-xl text-xl">
+                  <div className="absolute top-[95%] left-0 bg-[#F4F4F8] border-2 border-[#D9D9D9] text-[#202020] p-3 w-full text-left rounded-b-md text-ms z-10 m-0">
                     {beforReadingTips}
                   </div>
                 )}
               </button>
             </div>
           </div>
+          {/* 뉴스 이미지 */}
+          <div className='m-3'>
+            <img src="/newsImage3.png" alt="" className='w-full'/>
+          </div>
+          {/* 뉴스 내용 */}
+          <div className='text-xl m-3'>
+            안녕하세요. 여기에는 뉴스 내용이 들어 갈겁니다.
+            근데 아 이제 살짝 지겨운데 속도를 내보려하는데 이거 이거 사전기능 언제 구현할까요?
+            제가 할 수 있을까요...? 이제 버겁네여..^^
+          </div>
+        </div>
+
+        <div className="h-1.5 w-full bg-[#F5F6FA]"></div>
+
+        {/* 참여 컨텐츠 */}
+        <div>
+          <p className='m-5 text-2xl font-bold '>함께 참여해 볼까요?</p>
+          
+        </div>
+
+        <div className="h-1.5 w-full bg-[#F5F6FA]"></div>
+        <div>
+          <p className='m-5 text-2xl font-semibold'>이런 기사도 있어요!</p>
         </div>
       </div>
     </Layout>
