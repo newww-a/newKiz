df524f6ce85715402fc6c01b1a3d09bfcda30200
Ansoo <ansoo97@naver.com>
2025-03-26 17:57:07 +0900

    style: 마이페이지의 유저 프로필 UI 생성

--- front-end/src/pages/mypage/ui/MyPage.tsx
+++ front-end/src/pages/mypage/ui/MyPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { LuChevronLeft } from "react-icons/lu";
+import { UserProfile } from '@/widgets/mypage';
 
 export default function MyPage() {
   return (
-    <div>MyPage</div>
+    <div className="h-screen w-full flex flex-col">
+      <div className="w-[90%] h-16 flex flex-row justify-start items-center mx-auto">
+        <LuChevronLeft className="text-[25px]" />
+        <p className="text-2xl font-bold">마이페이지</p>
+      </div>
+      <div className="h-40 w-[90%] flex flex-row justify-center items-center mx-auto">
+        <UserProfile />
+      </div>
+      <div className="w-full h-1 bg-gray-100"></div>
+      <div className="w-[90%] mx-auto flex flex-col">
+        <p>어떤 카테고리에 관심이 많을까요?</p>
+      </div>
+    </div>
   )
 }
---
+++ front-end/src/widgets/mypage/ui/UserProfile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const UserProfile = () => {
+
+    const imgUrl: string = import.meta.env.VITE_AWS_S3_BASE_URL
+
+    return(
+        <div className="h-full w-full flex flex-row justify-center items-center gap-5">
+            <div className="flex rounded-full border border-gray-400 bg-white w-[90px] h-[90px] relative overflow-hidden justify-center items-center">
+                <img src={`${imgUrl}dinos/olaf.png`} alt="user character absolute"/>
+            </div>
+            <div className="flex flex-col justify-evenly h-2/3">
+                <div className="h-1/2 flex flex-col justify-end items-center">
+                    <p className="text-xl font-bold">zi승아zon</p>
+                </div>
+                <div className="h-1/2 flex justify-center">
+                    <p className="text-[#9e9e9e] font-semibold">화정남초등학교</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default React.memo(UserProfile);
