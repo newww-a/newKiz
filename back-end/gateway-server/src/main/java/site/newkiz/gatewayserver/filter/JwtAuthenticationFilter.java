809b3ce3c89645bce54c213ea9d4403aa9087c18
InJoo <jjoonior97@gmail.com>
2025-03-26 16:02:07 +0900

    feat: profile 없을 시 등록 페이지로 리다이렉트

--- back-end/gateway-server/src/main/java/site/newkiz/gatewayserver/filter/JwtAuthenticationFilter.java
+++ back-end/gateway-server/src/main/java/site/newkiz/gatewayserver/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,10 @@
 package site.newkiz.gatewayserver.filter;
 
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
@@ -8,14 +12,18 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import site.newkiz.gatewayserver.config.ApplicationConfig;
+import site.newkiz.gatewayserver.entity.ProfileRepository;
 import site.newkiz.gatewayserver.util.CookieUtil;
 import site.newkiz.gatewayserver.util.JwtUtil;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter implements WebFilter {
 
+  private final ApplicationConfig applicationConfig;
   private final JwtUtil jwtUtil;
   private final CookieUtil cookieUtil;
+  private final ProfileRepository profileRepository;
 
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
@@ -23,16 +31,24 @@ public class JwtAuthenticationFilter implements WebFilter {
       String accessToken = cookieUtil.getAccessToken(exchange.getRequest());
 
       if (accessToken != null) {
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-            jwtUtil.getId(accessToken), "", null);
+        Integer userId = jwtUtil.getId(accessToken);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userId, "", null);
 
-        return chain.filter(exchange)
-            .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
+        return profileRepository.findByUserId(userId)
+            .flatMap(profile -> chain.filter(exchange)
+                .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication)))
+            .switchIfEmpty(redirectToProfileSetup(exchange.getResponse()));
       }
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
 
     return chain.filter(exchange);
+  }
+
+  private Mono<Void> redirectToProfileSetup(ServerHttpResponse response) {
+    response.setStatusCode(HttpStatus.SEE_OTHER);
+    response.getHeaders().setLocation(URI.create("http://" + applicationConfig.getDomain() + "/userinfo"));
+    return response.setComplete();
   }
 }
