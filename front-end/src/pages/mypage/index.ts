5ba45b262995eefcac7f728fa3115779f75b5167
newww-a <tls001217@naver.com>
2025-04-06 01:59:08 +0900

    fix: build 오류 수정

--- front-end/src/pages/mypage/index.ts
+++ front-end/src/pages/mypage/index.ts
@@ -5,6 +5,6 @@ export { SummaryPage } from './ui/SummaryPage';
 export { WrongAnswerPage } from './ui/WrongAnswerPage';
 export { ScrappedNewsPage } from './ui/ScrappedNewsPage';
 export { ScrappedWordsPage } from './ui/ScrappedWordsPage';
-export { fetchMyPage, updateMyPage } from '../../pages/mypage/api/MypageApi';
+export { fetchMyPage, updateMyPage } from '../../pages/mypage/api/MyPageApi';
 export { fetchScrappedNews, fetchScrappedWords } from '../../pages/mypage/api/ScrapApi';
 export { fetchSummaries } from '../../pages/mypage/api/SummaryApi'
