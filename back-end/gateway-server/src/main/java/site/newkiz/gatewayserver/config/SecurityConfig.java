438da69c979fc8d8ac08adff251eb265f2f43e29
허인주 <jjoonior97@gmail.com>
2025-04-06 19:20:43 +0900

    Update back-end/gateway-server/src/main/java/site/newkiz/gatewayserver/config/SecurityConfig.java

--- back-end/gateway-server/src/main/java/site/newkiz/gatewayserver/config/SecurityConfig.java
+++ back-end/gateway-server/src/main/java/site/newkiz/gatewayserver/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package site.newkiz.gatewayserver.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
